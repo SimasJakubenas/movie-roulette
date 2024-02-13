f040fc9689749ea9911896e5949e3768436c6c28
Simas Jakubenas <sjakubenas@gmail.com>
2024-02-13 10:05:10 +0000

    Define spinRoulette function

--- static/js/materialize.js
+++ static/js/materialize.js
@@ -8,4 +8,34 @@ $(document).ready(function () {
         dist: -50,
         padding: 60,
     });
+    
+    spinRoulette()
+
 });
+
+/**
+ * Picks a random number and sets the carousel in motion
+ * Disables 'Spin It!!!' button and changes it's value
+ * Selects the carousel item
+ */
+function spinRoulette() {
+    $('#spin-it').on('click', function(e) {
+        let totalSlides = 0;
+        let oneSlide = setInterval(individualSpin, 300);
+        let randomPick = Math.floor(Math.random() * 10) + 5
+        $("#spin-it").attr("disabled", 'disabled')
+        $('#spin-it').html('In Motion!');
+        
+        // Traverses through carousel items and stops it based on random number
+        function individualSpin() {
+            $('.carousel').carousel('prev');
+            totalSlides++;
+            if (totalSlides > randomPick) {
+                clearInterval(oneSlide);
+                // Enables 'Spin It!!!' button and changes it's value back
+                $("#spin-it").removeAttr("disabled", 'disabled')
+                $('#spin-it').html('Spin It!!!');
+            }
+        }
+    })
+}
