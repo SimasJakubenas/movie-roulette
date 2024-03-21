2b98c5536abb270a7b83757c47687b06afd305cb
Simas Jakubenas <sjakubenas@gmail.com>
2024-03-21 15:23:02 +0000

    Add TV shows page

--- releases/urls.py
+++ releases/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('movies/', views.movie_releases, name='movie_releases')
+    path('movies/', views.movie_releases, name='movie_releases'),
+    path('shows/', views.show_releases, name='show_releases')
 ]
--- releases/views.py
+++ releases/views.py
@@ -1,7 +1,7 @@
 import requests
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from saved_viewings.views import API_KEY, BASE_URL, POSTER_BASE_URL, ENDPOINT_POPULAR_TITLES, DISCOVER_MOVIE
+from saved_viewings.views import API_KEY, BASE_URL, POSTER_BASE_URL, ENDPOINT_POPULAR_TITLES, DISCOVER_MOVIE, DISCOVER_SHOW
 from accounts.models import Profile
 
 
@@ -31,6 +31,39 @@ def movie_releases(request):
             "movies_discover": movies_discover,
             "movies_popular": movies_popular,
             "movies_top_rated": movies_top_rated,
+            "profile_data": profile_data,
+            "user_data": user_data,
+            "POSTER_BASE_URL": POSTER_BASE_URL
+        }
+    )
+
+
+def show_releases(request):
+
+    url_discover = f"{BASE_URL}{DISCOVER_SHOW}?api_key={API_KEY}&{ENDPOINT_POPULAR_TITLES}"
+    url_popular = f"{BASE_URL}/tv/popular?api_key={API_KEY}&{ENDPOINT_POPULAR_TITLES}"
+    url_top_rated = f"{BASE_URL}/tv/top_rated?api_key={API_KEY}&{ENDPOINT_POPULAR_TITLES}"
+    headers = {
+        "accept": "application/json",
+    }
+    response_discover = requests.get(url_discover, headers=headers)
+    response_popular = requests.get(url_popular, headers=headers)
+    response_top_rated = requests.get(url_top_rated, headers=headers)
+
+    shows_discover = response_discover.json()['results']
+    shows_popular = response_popular.json()['results']
+    shows_top_rated = response_top_rated.json()['results']
+
+    user_data = User.objects.get(pk=request.user.id)
+    profile_data = Profile.objects.get(user_id=request.user.id)
+
+    return render(
+        request,
+        "releases/shows.html",
+        {
+            "shows_discover": shows_discover,
+            "shows_popular": shows_popular,
+            "shows_top_rated": shows_top_rated,
             "profile_data": profile_data,
             "user_data": user_data,
             "POSTER_BASE_URL": POSTER_BASE_URL
